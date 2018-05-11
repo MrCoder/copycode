ac5ade38babb9b29156b3d007778ae6fb9b08f10
PengXiao <eagle.xiao@gmail.com>
2018-05-11 22:31:51 +1000

    Moved code into scripts.babel.

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+function enable() {
+  // add the button to any code portion
+  $('pre > code').not('.injected').addClass('injected')
+    .before($('<span class="copy-button">copy</span>'))
+    .parent().wrap('<div class="precontainer"></div>');
+  // do the magic with clipboard.js
+  new ClipboardJS('.copy-button', {
+    text: function (trigger) {
+      return $(trigger.nextElementSibling).text();
+    }
+  })
+    .on('success', function (e) {
+      $(e.trigger).html('copied')
+    })
+    .on('error', function (e) {
+      $(e.trigger).html('error')
+    });
+}
+enable();
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.greeting === 'hello')
+      enable();
+    sendResponse({farewell: 'copy code re-enabled.'});
+  });
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+document.getElementById('enable-button').onclick = enableCopyButton;
+function enableCopyButton() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: 'hello'}, function(response) {
+      console.log(response.farewell);
+    });
+  });
+}
